0c1faea178654c3cd3bbf78c2241387cea021259
moppiiiii <tnktmhssrc@gmail.com>
2023-08-09 15:48:38 +0900

    トレイリングスラッシュの制御確認

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // 'always' | 'never' | 'ignore'
+    // 'always' - 末尾にスラッシュを含むURLにのみマッチする (例: “/foo/“)
+    // 'never' - 末尾にスラッシュを含むURLにはマッチしない (ex: “/foo”)
+    // 'ignore' - URL の末尾に ”/” があるかどうかに関係なく一致する
+    trailingSlash: 'ignore',
+});
